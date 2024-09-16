aefecf6362434a2be0c6b7295d2a80861f6f1453
Mustafa Emir <mustafaemiryerlikaya@gmail.com>
2024-09-16 16:22:16 +0300

    quiz page icerigi duzenlendi + questioncard props gecıldi

---
+++ quizapp/src/components/questionCard/QuestionCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './QuestionCard.css';
+
+const QuestionCard = ({QuestionCard, score, setScore, count, setCount, modal, setModal}) => {
+  return (
+    <div>QuestionCard</div>
+  )
+}
+
+export default QuestionCard
--- quizapp/src/pages/quiz/Quiz.jsx
+++ quizapp/src/pages/quiz/Quiz.jsx
@@ -1,24 +1,38 @@
 import React, { useEffect, useState } from "react";
-import * as api from '../../api/api';
+import * as api from "../../api/api";
 import { useParams } from "react-router-dom";
 import "./Quiz.css";
+import QuestionCard from "../../components/questionCard/QuestionCard";
 
 const Quiz = () => {
   const { difficulty, amount } = useParams();
   const [questionsData, setQuestionsData] = useState([]);
+  const [score, setScore] = useState(0);
+  const [count, setCount] = useState(0); 
+  const [modal, setModal] = useState(false);
+
   useEffect(() => {
-    const getData = async() => {
+    const getData = async () => {
       const data = await api.fetchQuizData(difficulty, amount);
       setQuestionsData(data);
-    }
+    };
     getData();
-  }, [])
+  }, []);
 
-  console.log(questionsData)
-  
+  console.log(questionsData);
+
   return (
     <div className="quiz">
       {difficulty} - {amount}
+      <QuestionCard 
+      questionsData={questionsData} 
+      score={score}
+      setScore={setScore}
+      count={count}
+      setCount={setCount}
+      modal={modal}
+      setModal={setModal}
+      />
     </div>
   );
 };
