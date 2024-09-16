98ca4d453637acd4a8493844038f74f1542832e4
Mustafa Emir <mustafaemiryerlikaya@gmail.com>
2024-09-16 16:13:20 +0300

    api get işlemleri yapıldı + dogru cevap random yer degistirildi

--- quizapp/src/pages/quiz/Quiz.jsx
+++ quizapp/src/pages/quiz/Quiz.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
-import './Quiz.css';
+import React, { useEffect, useState } from "react";
+import * as api from '../../api/api';
+import { useParams } from "react-router-dom";
+import "./Quiz.css";
 
 const Quiz = () => {
-  const {difficulty, amount} = useParams();
+  const { difficulty, amount } = useParams();
+  const [questionsData, setQuestionsData] = useState([]);
+  useEffect(() => {
+    const getData = async() => {
+      const data = await api.fetchQuizData(difficulty, amount);
+      setQuestionsData(data);
+    }
+    getData();
+  }, [])
+
+  console.log(questionsData)
+  
   return (
-    <div className='quiz'>
+    <div className="quiz">
       {difficulty} - {amount}
     </div>
-  )
-}
+  );
+};
 
-export default Quiz
+export default Quiz;
